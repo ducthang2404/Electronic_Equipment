085db6d95117f93d60317415e54950d889abf867
ducthang2404 <ducthang12c8@gmail.com>
2022-02-21 21:23:03 +0700

    push git

---
+++ src/main/java/com/electronic/ElectronicEquipment1Application.java
@@ -0,0 +1,13 @@
+package com.electronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicEquipment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicEquipment1Application.class, args);
+	}
+
+}
