5bbcbb9195483528abc966d6eff5ef222e81f29c
ducthang2404 <ducthang12c8@gmail.com>
2022-03-03 18:39:47 +0700

    push

--- src/main/java/com/electronic/controller/HomeController.java
+++ src/main/java/com/electronic/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.electronic.entity.Category;
@@ -40,7 +41,7 @@ public class HomeController {
 		
 		List<Category> list = categoryService.findAll();
 		model.addAttribute("lstCategory", list);
-		
 		return "redirect:/product/list/" +1;
 	}
+
 }
